797509fbf1aa16b8ec3b42a0540310fe701196ee
miseon <miseon920@naver.com>
2022-09-20 12:01:15 +0900

    20220920

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,20 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">main</Link>
+          {/* <Link to="/">main</Link> */}
+          <NavLink to="/">main</NavLink>
         </li>
         <li>
-          <Link to="/sub01">Sub01</Link>
+          <NavLink to="/sub01">Sub01</NavLink>
         </li>
         <li>
-          <Link to="/sub02">Sub02</Link>
+          <NavLink to="/sub02">Sub02</NavLink>
         </li>
       </ul>
     </nav>
   );
 };
 export default Header;
+/*
+NavLink 컴포넌트는 링크에서 사용하는 경로가 
+현재 라우트의 경로와 일치하는 경우 
+특정 스타일 또는 CSS 클래스를 적용하는 컴포넌트입니다.
+active가 붙음
+*/
